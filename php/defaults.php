500f3dd747b5cfc6787d4e8b1e010105d2ac9b92
Mika Tuupola <tuupola@appelsiini.net>
2008-04-22 10:26:50 +0000

    Improved defaults.

--- php/defaults.php
+++ php/defaults.php
@@ -2,14 +2,14 @@
 
 /* these are just defaults for demonstrating purposes */
 
-$default['markitup_1'] = 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.';
-$default['markitup_2'] = $default['markitup_1'];
+$default['markitup_1'] = '<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+<h3>Your title here...</h3>
+<p>Duis autem vel eum in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto.</p>
+';
 
-$default['markitup_3'] = 'Lorem ipsum dolor sit amet, consectetuer adipiscing _elit, sed diam nonummy nibh_ euismod *tincidunt ut laoreet dolore magna aliquam* erat volutpat.
+$default['markitup_2'] = 'Lorem ipsum dolor sit amet, consectetuer adipiscing _elit, sed diam nonummy nibh_ euismod *tincidunt ut laoreet dolore magna aliquam* erat volutpat.
 
 * Foo
 * Bar
-
 Duis autem vel eum "edit in place":http://www.appelsiini.net/projects/jeditable  in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto.
-'
-?>
+';
